226e8e09e061c76f1ed784c78d37b299a1d77dd6
Koti P <kotip@Kotis-MacBook-Pro.local>
2022-10-31 16:23:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test by Koti
         </p>
         <a
           className="App-link"
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>
+        Hi there, this is koti. Testing 31-oct-2022 12:32 pm ( changes reflecting at runtime)
+      </p>
     </div>
   );
 }
